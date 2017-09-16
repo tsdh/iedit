5b14cc9fcaef509c50f25cff872fba5d70b2c799
Victor Ren <victorhge@gmail.com>
2017-09-17 01:24:05 +0800

    Add a demo gif and update document

--- iedit.el
+++ iedit.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2010, 2011, 2012 Victor Ren
 
-;; Time-stamp: <2017-09-15 00:07:02 Victor Ren>
+;; Time-stamp: <2017-09-16 21:05:28 Victor Ren>
 ;; Author: Victor Ren <victorhge@gmail.com>
 ;; Keywords: occurrence region simultaneous refactoring
 ;; Version: 0.9.9.9
@@ -28,42 +28,58 @@
 
 ;;; Commentary:
 
-;; This package is an Emacs minor mode and allows you to edit one occurrence of
-;; some text in a buffer (possibly narrowed) or region, and simultaneously have
-;; other occurrences edited in the same way.
-;;
-;; Normal scenario of iedit-mode is like:
-;;
-;; - Highlight certain contents - by press C-; (The default key binding)
-;;   All occurrences of a symbol, string in the buffer or a region may be
-;;   highlighted corresponding to current mark, point and prefix argument.
-;;   Refer to the document of `iedit-mode' for details.
-;;
-;; - Edit one of the occurrences
-;;   The change is applied to other occurrences simultaneously.
-;;
-;; - Finish - by pressing C-; again
-;;
+;; This package includes Emacs minor modes (iedit-mode and
+;; iedit-rectangle-mode) based on a API library (iedit-lib) and allows you to edit
+;; one occurrence of some text in a buffer (possibly narrowed) or region, and
+;; simultaneously have other occurrences edited in the same way, with visual
+;; feedback as you type.
+
+;; Normal work flow of Iedit mode is like:
+
+;;  - Move to certain point and press C-; (The default key binding).  All
+;;    occurrences of a symbol, string or a region in the buffer are highlighted
+;;    corresponding to the thing under the point, current mark and prefix argument.
+;;    Refer to the document of `iedit-mode' for details.
+
+;;  - Edit one of the occurrences
+;;    The change is applied to other occurrences simultaneously.
+
+;;  - Finish - by pressing C-; again
+
+;; Many other work flows to highlight occurrences are possible, for example,
+;; activation from isearch, incremental selection and markup tag pair selection.
+
 ;; You can also use Iedit mode as a quick way to temporarily show only the
 ;; buffer lines that match the current text being edited.  This gives you the
 ;; effect of a temporary `keep-lines' or `occur'.  To get this effect, hit C-'
 ;; when in Iedit mode - it toggles hiding non-matching lines.
-;;
+
 ;; Renaming refactoring is convenient in Iedit mode
-;;
-;; - The symbol under point is selected as occurrence by default and only
-;;   complete symbols are matched
-;; - With digit prefix argument 0, only symbols in current function are matched
-;; - Restricting symbols in current region can be done by pressing C-; again
-;; - Last renaming refactoring is remembered and can be applied to other buffers
-;;   later
-;;
+
+;;  - The symbol under point is selected as occurrence by default and only complete
+;;    symbols are matched
+
+;;  - With digit prefix argument 0, only occurrences in current function are matched
+
+;;  - Restricting symbols in current region can be done by pressing C-; again
+
+;;  - Last renaming refactoring is remembered and can be applied to other buffers
+;;    later
+
+;;  - Restricting the search area to just the current line can be done by
+;;    pressing M-I.
+
+;;  - Restricting the search area to the lines near the current line can
+;;    be done by pressing M-{ and M-}. These will expand the search
+;;    region one line at a time from the top and bottom.  Add a prefix
+;;    argument to go the opposite direction.
+
+;; Iedit-rectangle-mode provides rectangle support with *visible rectangle*
+;; highlighting, which is similar with cua mode rectangle support.  But it's
+;; lighter weight and uses iedit mechanisms.
+
 ;; There are also some other facilities you may never think about.  Refer to the
 ;; document of function `iedit-mode' (C-h f iedit-mode RET) for more details.
-
-;; The code was developed and fully tested on Gnu Emacs 24.0.93, partially
-;; tested on Gnu Emacs 22. If you have any compatible problem, please let me
-;; know.
 
 ;;; Contributors
 ;; Adam Lindberg <eproxus@gmail.com> added a case sensitivity option that can be toggled.
@@ -101,11 +117,11 @@ matches starts and end at word boundaries.")
 
 (defvar iedit-occurrence-type-local 'symbol
   "This is buffer local variable which indicates the occurrence
-type. It might be (symbol word other).")
+type. It might be (symbol word email url markup-tag regexp selection other).")
 
 (defvar iedit-occurrence-type-global 'symbol
   "This is global variable which indicates the last global occurrence
-type. It might be (symbol word other).")
+type. It might be (symbol word email url markup-tag regexp selection other).")
 
 (defvar iedit-last-occurrence-local nil
   "This is buffer local variable which is the occurrence when
